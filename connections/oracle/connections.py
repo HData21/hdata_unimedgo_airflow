702289d6c62d1f0d0bb40bfb934b3e02b3a8a38e
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-05-31 22:12:22 -0300

    Insert data to current_date - 1 (ALL) HML HUGYN

--- connections/oracle/connections.py
+++ connections/oracle/connections.py
@@ -11,22 +11,22 @@ def connect_ugo():
     dsn_tns = cx_Oracle.makedsn('10.64.25.41', 15120, service_name='dbtasy')  # 172.17.0.1
     return cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
 
-def connect_rhp_hdata():
+def connect_hdata():
     os.environ["NLS_LANG"] = ".UTF8"
     dsn_tns = cx_Oracle.makedsn('orclstage-1.cxp7emb18yqw.us-east-2.rds.amazonaws.com', 61521, service_name='orcl')
     return cx_Oracle.connect('UNIMED_GYN', 'UNIMEDGYN', dsn_tns)
 
 def connect():
-    connect_rhp_hdata_2 = 'oracle+cx_oracle://' + 'UNIMED_GYN' + ':' + 'UNIMEDGYN' + '@' + '/ORCL'
-    return connect_rhp_hdata_2
+    connect_hdata_2 = 'oracle+cx_oracle://' + 'UNIMED_GYN' + ':' + 'UNIMEDGYN' + '@' + '/ORCL'
+    return connect_hdata_2
 
 def engine():
     engine = create_engine(connect(), max_identifier_length=128)
     return engine
 
-def engine_rhp():
-    engine_rhp = create_engine(connect_rhp_hdata(), max_identifier_length=128)
-    return engine_rhp
+def engine():
+    engine = create_engine(connect_hdata(), max_identifier_length=128)
+    return engine
 
 Session = sessionmaker(bind=engine)
 session = Session()
