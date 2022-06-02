90bfab13d5a5311e392bba03dab387f12b5bb57b
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 01:48:45 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -4,6 +4,7 @@ import datetime
 import openpyxl
 import os
 import cx_Oracle
+import pyodbc
 
 from datetime import timedelta, date
 from dateutil import rrule
@@ -12,15 +13,22 @@ print("Path of the file..", os.path.abspath('ATENDIMENTO_PACIENTE.xlsx'))
 
 query_atendimento_paciente = "SELECT NR_ATENDIMENTO, NR_ATENDIMENTO_MAE, DT_ENTRADA, DT_INICIO_ATENDIMENTO, DT_ATEND_MEDICO, DT_ALTA, DT_ALTA_MEDICO, DT_FIM_TRIAGEM, NR_SEQ_TRIAGEM, DT_MEDICACAO, CD_MOTIVO_ALTA, CD_MOTIVO_ALTA_MEDICA, IE_TIPO_ATENDIMENTO, CD_PESSOA_FISICA, CD_MEDICO_RESP, NR_SEQ_PAC_SENHA_FILA, IE_CLINICA FROM TASY.VW_HDATA_ATENDIMENTO_PACIENTE WHERE DT_ENTRADA >= TO_DATE('{data_ini}', 'DD/MM/YYYY') AND DT_ENTRADA < TO_DATE('{data_fim}', 'DD/MM/YYYY') + INTERVAL '1' DAY"
 
-import pyodbc
-conn = pyodbc.connect('DRIVER={Oracle_test};Host=10.64.25.41;Port=15120;Service Name=dbtasy;User ID=hdata;Password=hdatats2022')
-# os.environ["NLS_LANG"] = ".UTF8"
-# dsn_tns = cx_Oracle.makedsn('10.64.25.41', 15120, service_name='dbtasy')
-# connect_ugo = cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
+os.environ["NLS_LANG"] = ".UTF8"
+dsn_tns = cx_Oracle.makedsn('10.64.25.41', 15120, service_name='dbtasy')
+connect_ugo = cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
+
+conn = pyodbc.connect('''
+        DRIVER={Oracle in OraDB19Home1};
+        DATABASE=10.64.25.41;
+        PORT=15120
+        UID=HDATA;
+        PWD=HDATATS2022;
+        SERVICE_NAME=dbtasy
+''')
 
 print("Entrou no df_atendimento_paciente")
 print(dt_ini.strftime('%d/%m/%Y'), ' a ', dt_ontem.strftime('%d/%m/%Y'))
-df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y'), data_fim=dt_ontem.strftime('%d/%m/%Y')), connect_ugo())
+df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y'), data_fim=dt_ontem.strftime('%d/%m/%Y')), conn())
 print(df_dim.info())
 
 df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]], index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
