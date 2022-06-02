e556faf91706656f635c60b34a1fe15a44eee4b8
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 01:23:22 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-print("Path of the file..", os.path.abspath('ATENDIMENTO_PACIENTE.txt'))
+print("Path of the file..", os.path.abspath('ATENDIMENTO_PACIENTE.xlsx'))
 
-df = pd.read_excel('/home/raphael.hdata/hdata_unimedgo_airflow/dags/ATENDIMENTO_PACIENTE.txt')
+df = pd.read_excel('/home/raphael.hdata/hdata_unimedgo_airflow/dags/ATENDIMENTO_PACIENTE.xlsx')
 print(df)
