eb83ea722ea3b04c15291a81a54bc1d39b38ffb0
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 03:57:48 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -17,7 +17,7 @@ dsn_tns = cx_Oracle.makedsn('10.64.25.41', 15120, service_name='dbtasy')
 connect_ugo = cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
 
 dt_ini = datetime.datetime(2021,12,23)
-dt_ontem = datetime.datetime(2022,5,31)
+dt_ontem = datetime.datetime(2022,6,1)
 
 print("Entrou no df_atendimento_paciente")
 for dt in rrule.rrule(rrule.MINUTELY, interval=30, dtstart=dt_ini, until=dt_ontem):
@@ -26,11 +26,11 @@ for dt in rrule.rrule(rrule.MINUTELY, interval=30, dtstart=dt_ini, until=dt_onte
 
     print(data_1.strftime('%d/%m/%Y %H:%M:%S'), ' a ', data_2.strftime('%d/%m/%Y %H:%M:%S'))
     
-    # df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=data_1.strftime('%d/%m/%Y %H:%M:%S'), data_fim=data_2.strftime('%d/%m/%Y %H:%M:%S')), connect_ugo)
-    # print(df_dim.info())
+    df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=data_1.strftime('%d/%m/%Y %H:%M:%S'), data_fim=data_2.strftime('%d/%m/%Y %H:%M:%S')), connect_ugo)
+    print(df_dim.info())
 
-    # compression_opts = dict(method='zip', archive_name='ATENDIMENTO_PACIENTE.csv')
-    # df_dim.to_csv('/home/raphael.hdata/hdata_unimedgo_airflow/dags/ATENDIMENTO_PACIENTE_'+ data_1.strftime('%d%m%Y%H%M%S') +'.zip', index=False, compression=compression_opts)
+    compression_opts = dict(method='zip', archive_name='ATENDIMENTO_PACIENTE.csv')
+    df_dim.to_csv('/home/raphael.hdata/hdata_unimedgo_airflow/dags/ATENDIMENTO_PACIENTE_'+ data_1.strftime('%d%m%Y%H%M%S') +'.zip', index=False, compression=compression_opts)
 
 print("DONE")
 
