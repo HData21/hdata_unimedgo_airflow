6cb872cf2f54a586a3273410d23b870af044f7bb
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 03:28:48 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -18,10 +18,10 @@ connect_ugo = cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
 
 print("Entrou no df_atendimento_paciente")
 dt_ini = datetime.datetime(2021,12,23, 0, 0, 0)
-dt_ontem = datetime.datetime(2021,12,23, 23, 59, 59)
+dt_ontem = datetime.datetime(2021,12,23, 11, 59, 59)
 print(dt_ini.strftime('%d/%m/%Y %H:%M:%S'), ' a ', dt_ontem.strftime('%d/%m/%Y %H:%M:%S'))
-print(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y %H:%M:%S'), data_fim=dt_ontem.strftime('%d/%m/%Y')))
-df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y %H:%M:%S'), data_fim=dt_ontem.strftime('%d/%m/%Y')), connect_ugo)
+print(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y %H:%M:%S'), data_fim=dt_ontem.strftime('%d/%m/%Y %H:%M:%S')))
+df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y %H:%M:%S'), data_fim=dt_ontem.strftime('%d/%m/%Y %H:%M:%S')), connect_ugo)
 print(df_dim.info())
 
 compression_opts = dict(method='zip', archive_name='ATENDIMENTO_PACIENTE.csv')
