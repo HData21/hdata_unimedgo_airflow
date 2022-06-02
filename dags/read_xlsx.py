b4b65bbb70027a589b5fbd744d907125f4d330c7
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 01:15:02 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_table(r'~/unimedgo_hdata-airflow/dags/ATENDIMENTO_PACIENTE.txt')
+df = pd.read_fwf('~/unimedgo_hdata-airflow/dags/ATENDIMENTO_PACIENTE.txt')
 print(df)
