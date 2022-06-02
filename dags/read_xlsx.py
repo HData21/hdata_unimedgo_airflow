e41741ed246d68a0979db6aef9bd3db4d640e3d7
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 03:23:19 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/read_xlsx.py
+++ dags/read_xlsx.py
@@ -10,16 +10,16 @@ from dateutil import rrule
 
 print("Path of the file..", os.path.abspath('ATENDIMENTO_PACIENTE.xlsx'))
 
-query_atendimento_paciente = "SELECT * FROM TASY.VW_HDATA_ATENDIMENTO_PACIENTE WHERE DT_ENTRADA >= TO_DATE('{data_ini}', 'DD/MM/YYYY') AND DT_ENTRADA < TO_DATE('{data_fim}', 'DD/MM/YYYY') + INTERVAL '1' DAY"
+query_atendimento_paciente = "SELECT * FROM TASY.VW_HDATA_ATENDIMENTO_PACIENTE WHERE DT_ENTRADA >= TO_DATE('{data_ini}', 'DD/MM/YYYY HH24:MI:SS') AND DT_ENTRADA < TO_DATE('{data_fim}', 'DD/MM/YYYY HH24:MI:SS') + INTERVAL '1' DAY"
 
 os.environ["NLS_LANG"] = ".UTF8"
 dsn_tns = cx_Oracle.makedsn('10.64.25.41', 15120, service_name='dbtasy')
 connect_ugo = cx_Oracle.connect('HDATA', 'HDATATS2022', dsn_tns)
 
 print("Entrou no df_atendimento_paciente")
-dt_ini = datetime.datetime(2021,12,23)
-dt_ontem = datetime.datetime(2021,12,23)
-print(dt_ini.strftime('%d/%m/%Y'), ' a ', dt_ontem.strftime('%d/%m/%Y'))
+dt_ini = datetime.datetime(2021,12,23, 1, 0, 0)
+dt_ontem = datetime.datetime(2021,12,23, 2, 0, 0)
+print(dt_ini.strftime('%d/%m/%Y %H:%M:%S'), ' a ', dt_ontem.strftime('%d/%m/%Y %H:%M:%S'))
 df_dim = pd.read_sql(query_atendimento_paciente.format(data_ini=dt_ini.strftime('%d/%m/%Y'), data_fim=dt_ontem.strftime('%d/%m/%Y')), connect_ugo)
 print(df_dim.info())
 
