43313e00ae787021352fda1ded8ab9ad061b7c96
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-06-02 00:35:57 -0300

    Teste arquivo excel HUGYN ATENDIMENTO_PACIENTE

--- dags/captura_dados.py
+++ dags/captura_dados.py
@@ -432,7 +432,7 @@ def df_atendimento_paciente():
     print(df_dim.info())
 
     writer = pd.ExcelWriter('ATENDIMENTO_PACIENTE.xlsx')
-    df_dim.to_excel(writer, sheet_name='sheet1')
+    df_dim.to_excel(writer)
     writer.save()
 
         # df_stage = pd.read_sql(query_atendimento_paciente_hdata.format(data_ini=data_1.strftime('%d/%m/%Y'), data_fim=data_2.strftime('%d/%m/%Y')), connect_hdata())
